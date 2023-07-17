39dec4b173c5fb8c00bb79bc3e87f6d2f895ec82
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-07-17 16:57:03 +0200

    second commit

--- src/com.goltsov.http/server/HttpServerRunner.java
+++ src/com.goltsov.http/server/HttpServerRunner.java
@@ -1,9 +1,9 @@
-package com.dmdev.http.server;
+package com.goltsov.http.server;
 
 public class HttpServerRunner {
 
     public static void main(String[] args) {
-        var httpServer = new HttpServer(9000, 100);
+        var httpServer = new com.dmdev.http.server.HttpServer(9000, 100);
         httpServer.run();
     }
 }
--- src/com.goltsov.http/servlet/FirstServlet.java
+++ src/com.goltsov.http/servlet/FirstServlet.java
@@ -6,11 +6,14 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.w3c.dom.ls.LSOutput;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
+import java.util.stream.Stream;
 
 @WebServlet("/first")
 public class FirstServlet extends HttpServlet {
@@ -38,8 +41,15 @@ public class FirstServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        var parameterMap = req.getParameterMap();
-        System.out.println(parameterMap);
+        /*var parameterMap = req.getParameterMap();
+        System.out.println(parameterMap);*/
+
+        try (BufferedReader reader = req.getReader();
+            Stream<String> lines = reader.lines()) {
+
+            lines.forEach(System.out::println);
+
+        }
     }
 
     @Override
